7a075cefbb44d3af479e03038a4e924f35aa55c5
4el0ve4ek <aksenoff.dany@yandex.ru>
2023-11-26 01:05:28 +0300

    some kind of parsing tokens

--- parser.fs
+++ parser.fs
@@ -8,6 +8,8 @@ type expr =
 | Lam of id*expr
 | Var of id
 | Int of int
+| String of string
+| None
 | PFunc of id
 | Cond of expr*expr*expr
 | Let of id*expr*expr
@@ -27,27 +29,32 @@ let funof = function
 | ">" -> (function [Int(a);Int(b)] -> if b>a then Int(1) else Int(0))
 | "<" -> (function [Int(a);Int(b)] -> if b<a then Int(1) else Int(0))
 | "<=" -> (function [Int(a);Int(b)] -> if b<=a then Int(1) else Int(0))
+| "print" -> (function  [expr.String(a); _] -> printfn "%s" a; None) // not work
+
 
 let rec eval exp env =
-  match exp with
-  | Int(n) -> Int(n)
-  | Var(x) -> Map.find x env
-  | Lam(id,ex) -> Closure(exp,env)
-  | App(ex1,ex2) -> apply (eval ex1 env) (eval ex2 env)
-  | PFunc(id) -> Op(id,2,[])
-  | Cond(e0,e1,e2) ->
-     if eval e0 env=Int(0) then eval e2 env else eval e1 env
-  | Let(id,e1,e2) ->
-     let e1' = eval e1 env in eval e2 (Map.add id e1' env)
-  | LetRec(id,e1,e2) ->
-     let e1' = RClosure(e1,env,id) in eval e2 (Map.add id e1' env)   
+    match exp with
+    | Int(n) -> Int(n)
+    | Var(x) -> Map.find x env
+    | Lam(id,ex) -> Closure(exp,env)
+    | App(ex1,ex2) -> apply (eval ex1 env) (eval ex2 env)
+    | PFunc(id) -> Op(id,3,[])
+    | Cond(e0,e1,e2) ->
+        match eval e0 env with
+            | Int(0) -> eval e2 env
+            | String(""), None -> eval e2 env
+            | _ -> eval e1 env
+    | Let(id,e1,e2) ->
+        let e1' = eval e1 env in eval e2 (Map.add id e1' env)
+    | LetRec(id,e1,e2) ->
+        let e1' = RClosure(e1,env,id) in eval e2 (Map.add id e1' env)   
 and apply e1 e2 =
-  match e1 with
-  | Closure(Lam(v,e),env) -> eval e (Map.add v e2 env)
-  | RClosure(Lam(v,e),env, id) -> eval e (Map.add v e2 (Map.add id e1 env))
-  | Op(id,n,args) ->
-     if n=1 then (funof id)(e2::args)
-     else Op(id,n-1,e2::args)
+    match e1 with
+    | Closure(Lam(v,e),env) -> eval e (Map.add v e2 env)
+    | RClosure(Lam(v,e),env, id) -> eval e (Map.add v e2 (Map.add id e1 env))
+    | Op(id,n,args) ->
+        if n=1 then (funof id)(e2::args)
+        else Op(id,n-1,e2::args)
 
 eval (App(App(PFunc("+"),Int(1)),Int(2))) Map.empty
 
@@ -55,22 +62,22 @@ eval (App(Lam("x",Var("x")),Int(5))) Map.empty
 
 eval (App(Lam("x",App(App(PFunc("*"),Var("x")),Var("x"))),Int(5))) Map.empty
 
+
 let P = 
    LetRec("fact",
      Lam("x",
        Cond(App(App(PFunc("<="),Var("x")),Int(1)),
-         Int(1),
+         Var("x"),
          App(App(PFunc("*"),Var("x")),
             App(Var("fact"),App(App(PFunc("-"),Var("x")),Int(1)))))),
      App(Var("fact"),Int(5)))
 
 eval P Map.empty
 
-let text = System.IO.File.ReadAllText("factorial.fgo")
-
 type Token =
     | String of string
     | Action of string
+    | Number of string
     | OpenBracket | CloseBracket
     | OpenSection | CloseSection
     | Comma
@@ -79,48 +86,66 @@ let tokenize text =
     let tokenEnder = function
         | ')' -> true
         | '(' -> true
-        | x when Char.IsWhiteSpace(x) -> true
+        | h when Char.IsWhiteSpace(h) -> true
         | _ -> false
 
     let rec action' acc = function
         | [] -> acc, []
-        | x :: other when tokenEnder x -> acc, (x::other)
-        | x :: other -> action' (acc+x.ToString()) other
+        | h :: t when tokenEnder h -> acc, (h::t)
+        | h :: t -> action' (acc+h.ToString()) t
+
+    let rec number' acc = function
+        | [] -> acc, []
+        | h :: t when Char.IsDigit(h) -> action' (acc+h.ToString()) t
+        | h :: t -> acc, (h::t)
 
     let rec string' acc = function
         | [] -> acc, []
-        | '\"' :: other -> acc, other
-        | x :: other when Char.IsWhiteSpace(x) -> string' acc other
-        | x :: other -> string' (acc+x.ToString()) other
+        | '\"' :: t -> acc, t
+        | x :: t when Char.IsWhiteSpace(x) -> string' acc t
+        | x :: t -> string' (acc+x.ToString()) t
     
     let rec tokenize' acc = function
         | [] -> Seq.rev acc
-        | '(' :: other -> tokenize' (OpenBracket::acc) other
-        | ')' :: other -> tokenize' (CloseBracket::acc) other
-        | '{' :: other -> tokenize' (OpenSection::acc) other
-        | '}' :: other -> tokenize' (CloseSection::acc) other
-        | ',' :: other -> tokenize' (Comma::acc) other
-        | x :: other when Char.IsWhiteSpace(x) -> tokenize' acc other
-        | '\"' :: other -> 
-            let token, rem = string' "" other
+        | '(' :: t -> tokenize' (OpenBracket::acc) t
+        | ')' :: t -> tokenize' (CloseBracket::acc) t
+        | '{' :: t -> tokenize' (OpenSection::acc) t
+        | '}' :: t -> tokenize' (CloseSection::acc) t
+        | ',' :: t -> tokenize' (Comma::acc) t
+        | h :: t when Char.IsWhiteSpace(h) -> tokenize' acc t
+        | '\"' :: t -> 
+            let token, rem = string' "" t
             tokenize' (Token.String(token) :: acc) rem
-        | x :: other ->
-            let token, rem = action' (x.ToString()) other
+        | h :: t when Char.IsDigit(h) -> 
+            let token, rem = number' (h.ToString())  t
+            tokenize' (Token.Number(token)::acc) rem
+        | h :: t ->
+            let token, rem = action' (h.ToString()) t
             tokenize' (Token.Action(token) :: acc) rem 
             
     tokenize' [] text
 
-
 let parse tokens = 
-    let parse' acc = function
-        |
-        |
-        | 
+    let rec parse' = function
+        // | CloseSection :: rem -> acc, rem
+        | Token.Number(s) :: Action(func) :: Token.Number(s2) :: _ when func = "+" ->
+            expr.App(expr.App(expr.PFunc(func),expr.Int(s |> int)),expr.Int(s2 |> int))
+       
+        // | Action(func) :: t when func == "func" = 
+        //     let fname = match List.head t with 
+        //         | Action(fname) -> fname
+        //         | _ -> raise("aaaa")
+        //     let t' = List.tail t
+        //     if List.head t' != Token.OpenBracket then raise("aaaaa") 
+        // | _ -> raise("aaaaa")
 
-    parse' [] tokens
+    parse' tokens
 
 
+let text = System.IO.File.ReadAllText("factorial.fgo")
 let tokens = tokenize (text |> Seq.toList)
-let parsed = parse tokens
+let parsed = parse (tokens |> Seq.toList)
+parsed
 
 eval parsed Map.empty
+
